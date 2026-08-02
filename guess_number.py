02650f603310b25e3a4ec8aed67c12d12b28d178
Ytgt265166 <yutog2381@yahoo.co.jp>
2026-08-02 17:57:18 +0900

    Added guessing range

--- guess_number.py
+++ guess_number.py
@@ -1,20 +1,21 @@
 import random
 
 def guess_number():
-    number_to_guess = random.randint(1, 100)
+    range = 300
+    number_to_guess = random.randint(1, range)
     attempts = 0
     guessed = False
 
     print("Welcome to the Guess the Number Game!")
-    print("I have selected a number between 1 and 100. Can you guess it?")
+    print(f"I have selected a number between 1 and {range}. Can you guess it?")
 
     while not guessed:
         try:
             user_guess = int(input("Enter your guess: "))
             attempts += 1
 
-            if user_guess < 1 or user_guess > 100:
-                print("Please enter a number between 1 and 100.")
+            if user_guess < 1 or user_guess > range:
+                print(f"Please enter a number between 1 and {range}.")
             elif user_guess < number_to_guess:
                 print("Too low! Try again.")
             elif user_guess > number_to_guess:
